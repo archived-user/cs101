405e591c4755c1bd748e71617e2b0c6564155983
twytwy <weiyangtan@users.noreply.github.com>
2017-06-13 17:59:39 +0800

    Create q1.py

--- q1.py
+++ q1.py
@@ -8,5 +8,31 @@
 #
 # Click the "Test Run" button below to try running your code and see the output,
 # and click "Submit" to submit your answer.
-
 print 7 * 7 * 24 * 60
+
+# Write Python code to print out how far light travels 
+# in centimeters in one nanosecond.  Use the values
+# defined below.    
+# speed_of_light = 299792458   meters per second
+# centimeters = 100            one meter is 100 centimeters
+# nanosecond = 1.0/1000000000  one billionth of a second
+print 299792458 * 100 * (1.0/1000000000)
+
+# Given the variables defined here, write Python 
+# code that prints out the distance, in meters, 
+# that light travels in one processor cycle. 
+# speed_of_light in meters per second
+# cycles_per_second is 2.7 GHz
+speed_of_light = 299792458.0 
+cycles_per_second = 2700000000.0
+print speed_of_light * (1/cycles_per_second)
+
+# Write python code that defines the variable 
+# age to be your age in years, and then prints 
+# out the number of days you have been alive.
+age = 18
+days_a_year = 365
+print age * days_a_year
+
+# Define a variable, name, and assign to it a string that is your name.
+name = "john"
