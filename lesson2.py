a71ae8c6b2c89e9ec424cd5faf7ad6ecf67d050c
twytwy <weiyangtan@users.noreply.github.com>
2017-06-14 20:14:42 +0800

    Create lesson2.py

--- lesson2.py
+++ lesson2.py
@@ -45,10 +45,36 @@ print text.find('hoo')
 # Here are two example test cases:
 #text = 'all zip files are zipped' 
 # >>> 18
-# text = 'all zip files are compressed'
+#text = 'all zip files are compressed'
 # >>> -1
 text = "all zip files are zipped" 
 # ENTER CODE BELOW HERE
-
+first_zip = text.find('zip')
+second_zip = text.find('zip',first_zip + 1)
+print second_zip
 # IMPORTANT BEFORE SUBMITTING: 
 # You should only have one print command in your function
+
+# Given a variable, x, that stores the 
+# value of any decimal number, write Python 
+# code that prints out the nearest whole 
+# number to x.
+# If x is exactly half way between two 
+# whole numbers, round up, so
+# 3.5 rounds to 4 and 2.5 rounds to 3.
+# You may assume x is not negative.
+# Hint: The str function can convert any number into a string.
+# eg str(89) converts the number 89 to the string '89'
+# Along with the str function, this problem can be solved 
+# using just the information introduced in unit 1.
+# x = 3.14159 
+# >>> 3 (not 3.0)
+# x = 27.63 
+# >>> 28 (not 28.0)
+# x = 3.5 
+# >>> 4 (not 4.0)
+x = 3.14159
+#ENTER CODE BELOW HERE
+s = str(x+0.5)
+dot = s.find('.')
+print s[:dot]
