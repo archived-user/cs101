b56ac7638294d57e850e75a164d76da766512093
twytwy <weiyangtan@users.noreply.github.com>
2017-06-17 16:12:28 +0800

    Create lesson13.py

---
+++ lesson13.py
@@ -0,0 +1,75 @@
+# A list is symmetric if the first row is the same as the first column,
+# the second row is the same as the second column and so on. Write a
+# procedure, symmetric, which takes a list as input, and returns the
+# boolean True if the list is symmetric and False if it is not.
+def symmetric(square):
+    # ensure it is a square
+    for rows in square:
+        if len(rows) != len(square):
+            return False
+    
+    row_num = 0
+    while row_num < len(square):
+        count = 0
+        while count < len(square):
+            if square[row_num][count] != square[count][row_num]:
+                return False
+            count += 1
+        row_num += 1
+
+    return True
+
+print symmetric([[1, 2, 3],
+                [2, 3, 4],
+                [3, 4, 1]])
+#>>> True
+
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "fish"],
+                ["fish", "fish", "cat"]])
+#>>> True
+
+print symmetric([["cat", "dog", "fish"],
+                ["dog", "dog", "dog"],
+                ["fish","fish","cat"]])
+#>>> False
+
+print symmetric([[1, 2],
+                [2, 1]])
+#>>> True
+
+print symmetric([[1, 2, 3, 4],
+                [2, 3, 4, 5],
+                [3, 4, 5, 6]])
+#>>> False
+
+print symmetric([[1,2,3],
+                 [2,3,1]])
+#>>> False
+
+
+
+
+
+# The mean of a set of numbers is the sum of the numbers divided by the
+# number of numbers. Write a procedure, list_mean, which takes a list of numbers
+# as its input and return the mean of the numbers in the list.
+
+# Hint: You will need to work out how to make your division into decimal
+# division instead of integer division. You get decimal division if any of
+# the numbers involved are decimals.
+
+def list_mean(inlist):
+    total = sum(inlist)
+    return (1.0 * total) / len(inlist)
+
+print list_mean([1,2,3,4])
+#>>> 2.5
+print list_mean([1,3,4,5,2])
+#>>> 3.0
+#print list_mean([])
+#>>> ??? You decide. If you decide it should give an error, comment
+# out the print line above to prevent your code from being graded as
+# incorrect.
+print list_mean([2])
+#>>> 2.0
