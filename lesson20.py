133177aa317a52d10449f840384590745c7b1d66
twytwy <weiyangtan@users.noreply.github.com>
2017-06-20 21:08:51 +0800

    Create lesson20.py

---
+++ lesson20.py
@@ -0,0 +1,71 @@
+# Write a procedure, shift, which takes as its input a lowercase letter,
+# a-z and returns the next letter in the alphabet after it, with 'a' 
+# following 'z'.
+
+def shift(letter):
+    return chr(((ord(letter) - 96) % 26) + 97)
+
+
+print shift('a')
+#>>> b
+print shift('n')
+#>>> o
+print shift('z')
+#>>> a
+
+
+
+
+
+# Write a procedure, shift_n_letters which takes as its input a lowercase
+# letter, a-z, and an integer n, and returns the letter n steps in the
+# alphabet after it. Note that 'a' follows 'z', and that n can be positive,
+#negative or zero.
+
+def shift_n_letters(letter, n):
+    return chr(((ord(letter) - 97 + n) % 26) + 97)
+
+
+print shift_n_letters('s', 1)
+#>>> t
+print shift_n_letters('s', 2)
+#>>> u
+print shift_n_letters('s', 10)
+#>>> c
+print shift_n_letters('s', -10)
+#>>> i
+
+
+
+
+
+# Write a procedure, rotate which takes as its input a string of lower case
+# letters, a-z, and spaces, and an integer n, and returns the string constructed
+# by shifting each of the letters n steps, and leaving the spaces unchanged.
+# Note that 'a' follows 'z'. You can use an additional procedure if you
+# choose to as long as rotate returns the correct string.
+# Note that n can be positive, negative or zero.
+
+def shift_n_letters(letter, n):
+    return chr(((ord(letter) - 97 + n) % 26) + 97)
+
+def rotate(s, n):
+    out = ''
+    for character in s:
+        if character == ' ':
+            out += character
+        else:
+            out += shift_n_letters(character, n)
+    return out
+
+print rotate ('sarah', 13)
+#>>> 'fnenu'
+print rotate('fnenu',13)
+#>>> 'sarah'
+print rotate('dave',5)
+#>>>'ifaj'
+print rotate('ifaj',-5)
+#>>>'dave'
+print rotate(("zw pfli tfuv nfibj tfiivtkcp pfl jyflcu "
+                "sv rscv kf ivru kyzj"),-17)
+#>>> ???
